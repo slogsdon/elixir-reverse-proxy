22405effb7783a3a855acb27d7e6d43b08a34dea
Shane Logsdon <shane@shanelogsdon.com>
2015-09-27 17:55:29 -0400

    clean up config

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,13 @@
 use Mix.Config
 
-config :logger, :console,
-  level: :info,
-  format: "$date $time [$level] $metadata$message\n",
-  metadata: [:user_id]
-
 config :dogma,
   rule_set: Dogma.RuleSet.All,
   exclude: [
-    ~r(\Aexample/),
+    ~r(example/),
+    ~r(test/)
   ]
 
 config ReverseProxy,
-  cache: false,
-  upstreams: %{ "api." => ["localhost:4000"],
-                "slogsdon.com" => ["localhost:4001"] }
+  upstreams: %{ "api." => {ReverseProxyTest.SuccessPlug, []},
+                "example.com" => {ReverseProxyTest.SuccessPlug, []},
+                "badgateway.com" => ["localhost:1"] }
