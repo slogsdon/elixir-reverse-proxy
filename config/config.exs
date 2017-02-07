126b002cfc20a5fe2ba1068afa9a2e5ed76af50a
Shane Logsdon <shane@shanelogsdon.com>
2017-02-06 23:11:54 -0500

    cleanup

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,5 @@
 use Mix.Config
 
-config :dogma,
-  rule_set: Dogma.RuleSet.All,
-  exclude: [
-    ~r(example/),
-    ~r(test/)
-  ]
-
 config :reverse_proxy,
   upstreams: %{ "api." => {ReverseProxyTest.SuccessPlug, []},
                 "example.com" => {ReverseProxyTest.SuccessPlug, []},
