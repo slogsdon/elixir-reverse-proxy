06636ae7b239bdaefdc0de29f24267f896d3a3bf
David N. Welton <davidw@dedasys.com>
2017-06-20 09:51:43 -0700

    Utilize full URI's for proxy targets
    https://github.com/slogsdon/elixir-reverse-proxy/issues/14

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,9 @@
 use Mix.Config
 
-config :reverse_proxy,
-  upstreams: %{ "api." => {ReverseProxyTest.SuccessPlug, []},
-                "example.com" => {ReverseProxyTest.SuccessPlug, []},
-                "badgateway.com" => ["localhost:1"] }
+config(:reverse_proxy,
+       upstreams: %{
+         # You could add foobar.localhost to /etc/hosts to test this
+         "foobar.localhost" => ["http://www.example.com"],
+         "api." => {ReverseProxyTest.SuccessPlug, []},
+         "example.com" => {ReverseProxyTest.SuccessPlug, []},
+         "badgateway.com" => ["http://localhost:1"] })
